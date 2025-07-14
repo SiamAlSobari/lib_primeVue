7d3aa2966890f4e1ecfcfc3ec2097a4ff92b1211
YammD123 <siamqueen52@gmail.com>
2025-07-14 21:54:27 +0700

    feat(deps): add dependensi daisyui

--- src/pages/rootPage.vue
+++ src/pages/rootPage.vue
@@ -1,3 +1,4 @@
 <template>
-    <h1 class="text-3xl">Root Page</h1>
+    <h1 class="text-3xl text-shadow-blue-500">Root Page</h1>
+    <button class="btn btn-warning btn-soft">Warning</button>
 </template>
