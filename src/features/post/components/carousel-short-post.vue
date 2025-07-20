a1fae37c1ecfd244d553d7f862cde21afa0c5dbc
YammD123 <siamqueen52@gmail.com>
2025-07-20 17:02:42 +0700

    feat(post): add carousel short and video

---
+++ src/features/post/components/carousel-short-post.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import Carousel from "primevue/carousel";
+
+const carouselImages = [
+    {
+        id: 1,
+        title: "Pantai Tropis",
+        description: "Nikmati matahari dan pasir putih yang memukau.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 2,
+        title: "Pegunungan",
+        description: "Udara sejuk dan pemandangan alam yang asri.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+    {
+        id: 3,
+        title: "Kota Modern",
+        description: "Keindahan arsitektur dan kehidupan malam.",
+        imageUrl:
+            "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=",
+    },
+];
+</script>
+
+<template>
+    <Carousel
+        :value="carouselImages"
+        class="w-full"
+        :items="3"
+        :circular="true"
+        :autoplay="true"
+        :numVisible="5"
+        :numScroll="1"
+        :showIndicators="false"
+        :responsiveOptions="[
+            { breakpoint: '1024px', numVisible: 5, numScroll: 1 },
+            { breakpoint: '768px', numVisible: 4, numScroll: 1 },
+            { breakpoint: '560px', numVisible: 3, numScroll: 1 },
+        ]"
+    >
+        <template #item="slotProps">
+            <div class="card flex justify-content-center mx-2">
+                <img
+                    :src="slotProps.data.imageUrl"
+                    :alt="slotProps.data.title"
+                    class="w-48 rounded h-72 object-"
+                />
+            </div>
+        </template>
+    </Carousel>
+</template>
--- src/pages/rootPage.vue
+++ src/pages/rootPage.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { Button } from 'primevue';
-
+import CarouselShortPost from "../features/post/components/carousel-short-post.vue";
+import CarouselVideoPost from "../features/post/components/carousel-video-post.vue";
 </script>
 
 <template>
-  <p>asa</p>
-  <Button label="Danger" severity="danger" />
+    <div class="flex flex-col gap-2">
+        <CarouselVideoPost />
+        <div class="flex flex-col gap-2">
+            <h1 class="text-2xl font-bold  rounded-lg bg-yellow-600 p-2">Short Post</h1>
+            <CarouselShortPost />
+        </div>
+    </div>
 </template>
