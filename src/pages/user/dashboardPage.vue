115a2e67608aa120b9bdfa953fbf4f88a7cf1fd5
YammD123 <siamqueen52@gmail.com>
2025-07-23 22:03:53 +0700

    feat(): add ui tabs in dashboard route

--- src/pages/user/dashboardPage.vue
+++ src/pages/user/dashboardPage.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "primevue";
+</script>
+
 <template>
-    <p>sasasa</p>
+    <div>
+        <h1 class="text-2xl font-bold mb-4">Dashboard</h1>
+        <p class="mb-4">
+            Welcome to your dashboard! Here you can manage view your posts.
+        </p>
+        <Tabs class="w-full bg-transparent costum-tabs" value="1">
+            <TabList>
+                <Tab value="1">Video</Tab>
+                <Tab value="2">Shorts</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel value="1">
+                    <p>ass</p>
+                </TabPanel>
+                <TabPanel value="2">
+                    <p>asas</p>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+    </div>
 </template>
+
+<style scoped>
+.costum-tabs {
+    --p-tabs-tablist-background: transparent;
+    --p-tabs-tabpanel-background: transparent;
+
+    /* Ubah warna teks saat tab aktif */
+    --p-tabs-tab-active-color: #f97316; /* oranye Tailwind: orange-500 */
+
+    /* Ubah warna garis bawah/tab indicator aktif */
+    --p-tabs-active-bar-background: #f97316;
+}
+</style>
